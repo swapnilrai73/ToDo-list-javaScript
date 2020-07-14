fba850aab46a1f9fcb50faeae0da9a06a3cc4ce6
swapnilrai73 <swapnilrai705@gmail.com>
2020-07-14 21:18:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const submitForm = document.querySelector('.add');
+const addButton = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todos');
+const list = document.querySelectorAll('.todos li'); 
+
+let listLenght = list.lenght;
+
+const generateTempalate = (todo) => {
+  
+  const html = `<li>
+                  <input type="checkbox" id="todo_${listLenght}">
+                  <label for="todo_${listLenght}">
+                    <span class="check"></span>
+                    ${todo}
+                  </label>
+                  <i class="far fa-trash-alt delete"></i>
+                </li>`
+  todoList.innerHTML += html;
+};
+
+function addTodos(e) {
+  e.preventDefault();
+  const todo = submitForm.add.value.trim();
+  if (todo.length) {
+    listLenght = listLenght + 1;
+    generateTempalate(todo);
+    submitForm.reset();
+  }
+}
+
+submitForm.addEventListener('submit', addTodos);
+addButton.addEventListener('click', addTodos);
+
+function deleteTodos(e) {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+}
+
+todoList.addEventListener('click', deleteTodos);
